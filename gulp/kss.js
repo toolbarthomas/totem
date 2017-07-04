dcd4a857b56274fb5db4a70189e296d03c178089
Thomas van der Velde <contact@toolbarthomas.net>
2017-07-04 18:38:35 +0200

    KSS gulp generator file

--- gulp/kss.js
+++ gulp/kss.js
@@ -1,5 +1,27 @@
 module.exports = (GULP, PLUGINS, NODE_MODULES, PATHS, REVISION) => {
     return function (callback) {
-        return PLUGINS.run('./node_modules/.bin/kss --c ./styleguide.json').exec();
+        var sources = [
+            PATHS.src,
+            PATHS.packages
+        ];
+
+        var streams = [];
+
+        sources.forEach(function(source) {
+            NODE_MODULES.del([source + '/**/scss/**/*.html']).then(function() {
+                var stream = GULP.src([
+                    source + '/**/scss/**/*.twig',
+                ], {
+                    nodir: true
+                })
+                .pipe(PLUGINS.twig())
+                .pipe(PLUGINS.faker())
+                .pipe(GULP.dest(source));
+
+                streams.push(stream);
+            });
+        }, this);
+
+        return NODE_MODULES.merge(streams).pipe(PLUGINS.run('./node_modules/.bin/kss --c ./styleguide.json').exec());
     }
 }
