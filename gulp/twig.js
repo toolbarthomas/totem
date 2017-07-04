2a82bd4f5eeffb9e89c71f5a8d7b37e7c33dde1c
Thomas van der Velde <contact@toolbarthomas.net>
2017-07-04 18:37:48 +0200

    Twig generator

--- gulp/twig.js
+++ gulp/twig.js
@@ -1,33 +1,40 @@
 module.exports = (GULP, PLUGINS, NODE_MODULES, PATHS, REVISION) => {
     return function (callback) {
-        var twig_options = {
-            base: PATHS.src,
-            errorLogToConsole: true
-        }
+        var sources = [
+            {
+                input: [
+                    PATHS.src + '/**/*.twig',
+                    '!' + PATHS.src + '/**/assets/**/*.twig',
+                    '!' + PATHS.src + '/**/layouts/**/*.twig',
+                    '!' + PATHS.src + '/**/partials/*.twig',
+                ],
+                output: ''
+            },
+            {
+                input: [
+                    PATHS.packages + '/**/*.twig'
+                ],
+                output: '/submodules'
+            }
+        ];
 
-        var src = GULP.src([
-            PATHS.src + '/**/*.twig',
-            '!' + PATHS.src + '/**/assets/**/*.twig',
-            '!' + PATHS.src + '/**/layouts/**/*.twig',
-            '!' + PATHS.src + '/**/partials/*.twig',
-        ], {
-            nodir: true
-        })
-        .pipe(PLUGINS.plumber())
-        .pipe(PLUGINS.twig(twig_options))
-        .pipe(PLUGINS.faker())
-        .pipe(GULP.dest(PATHS.dest));
+        var streams = [];
 
-        var modules = GULP.src([
-            PATHS.modules + '/**/*.twig'
-        ], {
-            nodir: true
-        })
-        .pipe(PLUGINS.plumber())
-        .pipe(PLUGINS.twig(twig_options))
-        .pipe(PLUGINS.faker())
-        .pipe(GULP.dest(PATHS.dest + '/submodules' ));
+        sources.forEach(function(source) {
+            var stream = GULP.src(source.input, {
+                nodir: true
+            })
+            .pipe(PLUGINS.plumber())
+            .pipe(PLUGINS.twig({
+                base: PATHS.src,
+                errorLogToConsole: true
+            }))
+            .pipe(PLUGINS.faker())
+            .pipe(GULP.dest(PATHS.dest + source.output));
 
-        return NODE_MODULES.merge(src, modules);
+            streams.push(stream);
+        }, this);
+
+        return NODE_MODULES.merge(streams);
     }
 }
