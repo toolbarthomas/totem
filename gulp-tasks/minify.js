50b9cdf6866007dcc73ec7ceda3b2db87fad0cc1
Thomas van der Velde <contact@toolbarthomas.net>
2017-07-07 17:09:24 +0200

    Minify fixes

--- gulp-tasks/minify.js
+++ gulp-tasks/minify.js
@@ -2,27 +2,33 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, PATHS, REVISION) => {
     return function (callback)
     {
         var stylesheets = GULP.src([
-            PATHS.dest + '/assets/css/**/*.css'
-        ])
+            PATHS.dest + '/resources/**/stylesheets/**/*.css',
+            '!**/*.min*'
+        ], {
+            no_dir: true
+        })
         .pipe(PLUGINS.combineMq({
             beautify: false
         }))
-        .pipe(plugins.cssnano())
-        .pipe(plugins.rename({
+        .pipe(PLUGINS.cssnano())
+        .pipe(PLUGINS.rename({
             suffix: '.min'
         }))
-        .pipe(GULP.dest(PATHS.dest + '/assets/css/'));
+        .pipe(GULP.dest(PATHS.dest + '/resources' ));
 
         var javascripts = GULP.src([
-            '!**/modernizr/**',
-            PATHS.dest + '/assets/js/**/*.js',
-        ])
+            PATHS.dest + '/resources/**/javascripts/**/*.js',
+            '!' + PATHS.dest + '/resources/base/javascripts/lib/**/*',
+            '!**/*.min*'
+        ], {
+            no_dir: true
+        })
         .pipe(PLUGINS.uglify())
         .pipe(PLUGINS.rename({
             suffix: '.min'
         }))
-        .pipe(GULP.dest(PATHS.dest + '/assets/js/'));
+        .pipe(GULP.dest(PATHS.dest + '/resources' ));
 
-        return NODE_MODULES.merge(stylesheets, javascripts);
+        return NODE_MODULES.merge(javascripts);
     }
 }
