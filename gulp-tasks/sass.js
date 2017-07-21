10d84703938494fd4c5110dcc29fb6b7163d4d0c
Thomas van der Velde <contact@toolbarthomas.net>
2017-07-21 17:42:17 +0200

    Seperate stylesheets

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -4,21 +4,36 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, PATHS, REVISION) => {
         var sources = [
             {
                 input: [
-                    PATHS.src + '/resources/templates/*/stylesheets/*.scss'
+                    PATHS.src + '/resources/templates/*/stylesheets/*.scss',
                 ],
-                output: PATHS.dest + '/resources/templates'
+                output: PATHS.dest + '/resources/templates',
+                options: {
+                    ignorePaths: [
+                        '**/__*.scss'
+                    ]
+                }
             },
             {
                 input: [
                     PATHS.src + '/resources/pages/*/stylesheets/*.scss'
                 ],
-                output: PATHS.dest + '/resources/pages'
+                output: PATHS.dest + '/resources/pages',
+                options: {
+                    ignorePaths: [
+                        '**/__*.scss'
+                    ]
+                }
             },
             {
                 input: [
                     PATHS.src + '/resources/modules/*/stylesheets/*.scss'
                 ],
-                output: PATHS.dest + '/resources/modules'
+                output: PATHS.dest + '/resources/modules',
+                options: {
+                    ignorePaths: [
+                        '**/__*.scss'
+                    ]
+                }
             }
         ];
 
@@ -27,11 +42,7 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, PATHS, REVISION) => {
         sources.forEach(function (source) {
             var stream = GULP.src(source.input)
             .pipe(PLUGINS.sourcemaps.init())
-            .pipe(PLUGINS.sassGlob({
-                ignorePaths: [
-                    '**/__*.scss'
-                ]
-            }))
+            .pipe(PLUGINS.sassGlob(source.options))
             .pipe(PLUGINS.sass().on('error', PLUGINS.sass.logError))
             .pipe(PLUGINS.sourcemaps.write('./'))
             .pipe(GULP.dest(source.output))
