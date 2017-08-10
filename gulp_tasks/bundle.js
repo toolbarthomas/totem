c59f2051721c2fb07faa47b15a8ca89fadfe2e94
Thomas van der Velde <contact@toolbarthomas.net>
2017-08-10 16:52:51 +0200

    Fixes

--- gulp_tasks/bundle.js
+++ gulp_tasks/bundle.js
@@ -1,16 +1,20 @@
 module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
     return function (callback) {
 
+        // Define all module locations in a globbing pattern (including your external packages: bower, NPM etc.)
         var sources = [
-            process.env.SRC + '/resources/modules/*/javascripts/*.js'
+            // process.env.SRC + '/resources/modules/*/javascripts/*.js',
+            process.env.MODULES_PATH + '/totem.module.*/javascripts/*.js'
         ];
 
+        //Setup an empty bunddle were Browserify does its magic.
         var bundle = [];
+
+        //Define a stream we can return to complete our task
         var streams = [];
 
+        //Callback function to generate a seperate module
         function makeBundle() {
-            console.log(bundle);
-
             for (var index = 0; index < bundle.length; index++) {
                 var basename = NODE_MODULES.path.basename(bundle[index]);
                 var ext = NODE_MODULES.path.extname(basename);
@@ -20,11 +24,11 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
 
                 var queue = NODE_MODULES.browserify({
                     entries: basename,
-                    standalone: name
+                    standalone: NODE_MODULES.camelCase(name)
                 }).bundle()
                 .pipe(NODE_MODULES.vinylSourceStream(basename))
                 .pipe(PLUGINS.derequire())
-                .pipe(GULP.dest(destination));
+                .pipe(GULP.dest(process.env.DEST + '/resources/modules/' + name + '/javascripts'));
 
                 streams.push(queue);
             }
@@ -32,16 +36,17 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
             return NODE_MODULES.merge(streams);
         }
 
-
+        //Setup our bundle with our defined sources:
         for (var index = 0; index < sources.length; index++) {
             NODE_MODULES.glob(sources[index], function (error, file) {
                 if (error) {
                     return;
                 }
 
+                //Add the found files to our bunddle
                 bundle = bundle.concat(file);
 
-
+                //Callback
                 if(index == sources.length) {
                     makeBundle();
                 }
