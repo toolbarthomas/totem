7825b5417fc76df9b5d5a5b837f10d23830d201b
Thomas van der Velde <contact@toolbarthomas.net>
2017-10-31 13:33:15 +0100

    Twig fixes

--- gulp_tasks/twig.js
+++ gulp_tasks/twig.js
@@ -1,5 +1,7 @@
 module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
     return function (callback) {
+
+        // Iterate between each type of component withing the project.
         var sources = [
             {
                 input: [
@@ -12,6 +14,7 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
                 input: [
                     process.env.SRC + '/resources/modules/**/*.twig', // Project Modules
                     process.env.SUBMODULES_PATH + '/totem.module.*/**/*.twig', // External Modules,
+                    '!' + process.env.SUBMODULES_PATH + '/totem.module.*/**/node_modules/**/*.twig', // External Modules,
                     '!**/totem.module.*/**/bower_components/**', // Can remove if no package has bower
                     '!**/partials/**',
                     '!**/totem.module.tipi/**'
@@ -20,34 +23,69 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
             }
         ];
 
-        var streams = [];
+        // Define the global json file so can share data between multiple pages
+        var global_json = {
+            path: process.env.SRC + '/data.json',
+            data: {}
+        };
 
-        // Json file we can use to define global values within the project.
-        var global_json = {};
-        var data = process.env.SRC + '/data.json';
-        if (NODE_MODULES.fse.existsSync(data)) {
-            global_json = JSON.parse(NODE_MODULES.fse.readFileSync(data));
+        var data = {};
+
+        // Insert the data from the global data.json file within our json.data
+        if (NODE_MODULES.fse.existsSync(global_json.path)) {
+            data = JSON.parse(NODE_MODULES.fse.readFileSync(global_json.path));
         }
 
+        // Setup the streams where we push each Gulp instance
+        // And return the whole object all together
+        var streams = [];
+
+        // Iterate trough each source so we can transform all twig sources
+        // Return each iterated object into the stream.
         sources.forEach(function(source) {
+            // Abort if we have no inout within our source
+            if (source.input === 0) {
+                return;
+            }
+
             var stream = GULP.src(source.input, {
                 nodir: true
             })
             .pipe(PLUGINS.plumber())
-            .pipe(PLUGINS.data(function (file) {
-                var data = 'data.json';
+            .pipe(PLUGINS.data(function(file) {
 
-                var path = NODE_MODULES.path.relative(file.cwd, file.path);
-                var extension = NODE_MODULES.path.basename(path);
+                // Define the json file
+                var name = 'data.json';
+                var path = NODE_MODULES.path.parse(NODE_MODULES.path.relative(file.cwd, file.path));
 
-                var f = path.replace(extension, 'data.json');
+                // Define if we have an json file within our page
+                var json = {
+                    path: path.dir + '/' + name,
+                    data: false
+                };
 
-                // Check if data.json exists
-                if (!NODE_MODULES.fse.existsSync(f)) {
-                    return {};
+
+                // Check if we have a data json file within the same directory of the current file
+                if(!NODE_MODULES.fse.existsSync(json.path)) {
+                    // Change the current json path to it's parent
+                    // So we can check if our parent directory has an json file.
+                    json.path = path.dir + '/../' + name;
+
+                    // Check if our parent directory has a data json
+                    if (NODE_MODULES.fse.existsSync(json.path)) {
+                        json.data = true;
+                    }
+                } else {
+                    json.data = true;
                 }
 
-                return Object.assign(global_json, JSON.parse(NODE_MODULES.fse.readFileSync(f)));
+                // Return the json object if we have one
+                // Or Return an empty object if no json file has been found
+                if(json.data) {
+                    data = Object.assign(data, JSON.parse(NODE_MODULES.fse.readFileSync(json.path)));
+                }
+
+                return data;
             }))
             .pipe(PLUGINS.twig({
                 base: process.env.SRC + '/resources'
