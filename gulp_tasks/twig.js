4d243b7c8a359dc1f483cc32f916097c9ecded79
Thomas van der Velde <contact@toolbarthomas.net>
2017-11-27 16:52:34 +0100

    Use env variable namespaces

--- gulp_tasks/twig.js
+++ gulp_tasks/twig.js
@@ -41,6 +41,9 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
         // And return the whole object all together
         var streams = [];
 
+        // // Tmp cache location for json streams
+        // var tap_cache;
+
         // Iterate trough each source so we can transform all twig sources
         // Return each iterated object into the stream.
         sources.forEach(function(source) {
@@ -53,46 +56,52 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
                 nodir: true
             })
             .pipe(PLUGINS.plumber())
+            // .pipe(PLUGINS.tap(function(file) {
+            //     tap_cache = undefined;
+
+            //     // Define the json file
+            //     var name = 'data.json';
+            //     var path = NODE_MODULES.path.parse(NODE_MODULES.path.relative(file.cwd, file.path));
+
+            //     // Define if we have an json file within our page
+            //     var json = {
+            //         path: path.dir + '/' + name,
+            //         data: false
+            //     };
+
+
+            //     // Check if we have a data json file within the same directory of the current file
+            //     if(!NODE_MODULES.fse.existsSync(json.path)) {
+            //         // Change the current json path to it's parent
+            //         // So we can check if our parent directory has an json file.
+            //         json.path = path.dir + '/../' + name;
+
+            //         // Check if our parent directory has a data json
+            //         if (NODE_MODULES.fse.existsSync(json.path)) {
+            //             json.data = true;
+            //         }
+            //     } else {
+            //         json.data = true;
+            //     }
+
+            //     // Return the json object if we have one
+            //     // Or Return an empty object if no json file has been found
+            //     if(json.data) {
+            //         console.log(typeof data);
+            //         tap_cache = {
+            //             app: Object.keys(data)[0],
+
+            //         }JSON.parse(NODE_MODULES.fse.readFileSync(json.path)));
+            //     }
+            // }))
             .pipe(PLUGINS.data(function(file) {
-
-                // Define the json file
-                var name = 'data.json';
-                var path = NODE_MODULES.path.parse(NODE_MODULES.path.relative(file.cwd, file.path));
-
-                // Define if we have an json file within our page
-                var json = {
-                    path: path.dir + '/' + name,
-                    data: false
-                };
-
-
-                // Check if we have a data json file within the same directory of the current file
-                if(!NODE_MODULES.fse.existsSync(json.path)) {
-                    // Change the current json path to it's parent
-                    // So we can check if our parent directory has an json file.
-                    json.path = path.dir + '/../' + name;
-
-                    // Check if our parent directory has a data json
-                    if (NODE_MODULES.fse.existsSync(json.path)) {
-                        json.data = true;
-                    }
-                } else {
-                    json.data = true;
-                }
-
-                // Return the json object if we have one
-                // Or Return an empty object if no json file has been found
-                if(json.data) {
-                    data = Object.assign(data, JSON.parse(NODE_MODULES.fse.readFileSync(json.path)));
-                }
-
                 return data;
             }))
             .pipe(PLUGINS.twig({
                 base: './',
                 namespaces: {
-                    'totem_submodules': '../totem_submodules/',
-                    'totem': '../totem/'
+                    'totem_submodules': NODE_MODULES.path.normalize(process.env.TOTEM_SUBMODULES) + '/',
+                    'totem': '../' + (process.cwd()).substring((process.cwd()).lastIndexOf("/") + 1) + '/'
                 },
                 onError: function(error) {
                     if(!error) {
