a95444778d1cc49d46ae7fd8a08559ed71a0b756
Thomas van der Velde <contact@toolbarthomas.net>
2017-12-07 10:32:14 +0100

    Glob fix twig

--- gulp_tasks/twig.js
+++ gulp_tasks/twig.js
@@ -7,7 +7,6 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
                 input: [
                     process.env.TOTEM_SRC + '/resources/groups/**/*.twig',
                     process.env.TOTEM_SUBMODULES + '/totem.group*/**/*.twig',
-                    '!**/groups/**/partials/**/*.twig',
                     '!' + process.env.TOTEM_SUBMODULES + '/**/node_modules/**/*.twig'
                 ],
                 output: process.env.TOTEM_DEST + '/resources/groups'
@@ -16,8 +15,9 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
                 input: [
                     process.env.TOTEM_SRC + '/resources/modules/**/*.twig',
                     process.env.TOTEM_SUBMODULES + '/totem.module*/**/*.twig',
-                    '!**/modules/**/partials/**/*.twig',
-                    '!' + process.env.TOTEM_SUBMODULES + '/**/node_modules/**/*.twig'
+                    '!' + process.env.TOTEM_SUBMODULES + '/**/node_modules/**/*.twig',
+                    '!' + process.env.TOTEM_SRC + '/resources/modules/**/partials/**/*.twig',
+                    '!' + process.env.TOTEM_SUBMODULES + '/totem.module*/**/partials/**/*.twig',
                 ],
                 output: process.env.TOTEM_DEST + '/resources/modules'
             }
@@ -25,7 +25,7 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
 
         // Define the global json file so can share data between multiple pages
         var global_json = {
-            path: process.env.TOTEM_SRC + '/data.json',
+            path: process.cwd() + '/data.json',
             data: {}
         };
 
@@ -56,41 +56,56 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
             })
             .pipe(PLUGINS.plumber())
             // .pipe(PLUGINS.tap(function(file) {
-            //     tap_cache = undefined;
-
-            //     // Define the json file
-            //     var name = 'data.json';
             //     var path = NODE_MODULES.path.parse(NODE_MODULES.path.relative(file.cwd, file.path));
 
-            //     // Define if we have an json file within our page
             //     var json = {
-            //         path: path.dir + '/' + name,
+            //         path: path.dir + 'data.json',
             //         data: false
             //     };
 
 
-            //     // Check if we have a data json file within the same directory of the current file
-            //     if(!NODE_MODULES.fse.existsSync(json.path)) {
-            //         // Change the current json path to it's parent
-            //         // So we can check if our parent directory has an json file.
-            //         json.path = path.dir + '/../' + name;
+            //     // Check if there is a data.json file within the current directory of the current file
+            //     // Try to find the json file within the parent directory if can't find it within the original path
+            //     if (!NODE_MODULES.fse.existsSync(json.path)) {
+            //         // Update json path to the parent path
+            //         json.path = path.dir + '/../' + 'data.json';
 
-            //         // Check if our parent directory has a data json
             //         if (NODE_MODULES.fse.existsSync(json.path)) {
             //             json.data = true;
             //         }
-            //     } else {
-            //         json.data = true;
             //     }
 
-            //     // Return the json object if we have one
-            //     // Or Return an empty object if no json file has been found
-            //     if(json.data) {
-            //         console.log(typeof data);
-            //         tap_cache = {
-            //             app: Object.keys(data)[0],
+            //     // Abort if we have no data
+            //     if (!json.data) {
+            //         return;
+            //     }
 
-            //         }JSON.parse(NODE_MODULES.fse.readFileSync(json.path)));
+            //     var array = JSON.parse(NODE_MODULES.fse.readFileSync(json.path));
+
+            //     if (typeof array != 'object') {
+            //         return;
+            //     }
+
+            //     // Filter out empty arrays
+            //     if(array.length == 0) {
+            //         return;
+            //     }
+
+            //     console.log(typeof data);
+            //     console.log(typeof array);
+
+
+            //     for (var key in array) {
+            //         // Skip undefined keys
+            //         if(key == null) {
+            //             continue;
+            //         }
+
+            //         if (data.hasOwnProperty(key)) {
+            //             continue;
+            //         }
+
+            //         data = Object.assign(data, array[key]);
             //     }
             // }))
             .pipe(PLUGINS.data(function(file) {
@@ -106,8 +121,6 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
                     if(!error) {
                         return;
                     }
-
-                    console.log(error);
                 }
             }))
             .pipe(PLUGINS.faker())
